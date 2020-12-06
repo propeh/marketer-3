b6bfc5753449b8e1c9df2f1e26201ce7016556d8
propeh <propeh@hsit.co.kr>
2020-12-06 22:42:42 +0900

    marketer 다시만들기 - section 작성중

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// 자바스크립트 기본 준비 함수
+// window.addEventListener('DomContentRoaded', function (){
+//
+//     marketer.init()
+// })
+
+// marketer 함수들
+// const marketer = {
+//     init : function () {
+//         marketer.faq()
+//     },
+//     faq : function () {
+//         document.querySelector('.faq.question').addEventListener('click', function (){
+//             this.classList('is-active')
+//         })
+//     }
+// }
+
+// 제이쿼리 기본 준비 함수
+$(function (){
+    marketer.init()
+})
+
+const marketer = {
+    init : function () {
+        marketer.faq()
+    },
+    faq : function () {
+        $('.faq li').on('click', function () {
+            const faqIndex = $(this).index();
+            $('.faq li').eq(faqIndex).addClass('is-active').siblings().removeclass('is-active');
+        })
+    }
+}
+
